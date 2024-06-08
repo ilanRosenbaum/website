f21de94f4789f932965427afbf983a2319a356ff
IlanRosenbaum <ilan@al.net>
2024-06-08 15:43:39 -0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello, React with Tailwind CSS and TypeScript!</h1>
+    </div>
+  );
+}
+
+export default App;
