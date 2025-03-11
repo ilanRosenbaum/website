d3baa7a1c6c24a44e4ad243d9db3a82896327367
IlanRosenbaum <ilan@al.net>
2025-03-11 10:24:52 -0500

    Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "",
+  authDomain: "ilan-web.firebaseapp.com",
+  projectId: "ilan-web",
+  storageBucket: "ilan-web.appspot.com",
+  messagingSenderId: "870074135993",
+  appId: "1:870074135993:web:91b3fabb9cd02cc3153268",
+  measurementId: "G-RSBLK5DW2W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export { storage, firebaseConfig, analytics };
