bb0cf7cfa1125a203882c620a004b85a0d8f0c5e
IlanRosenbaum <ilan@al.net>
2025-12-28 14:20:02 -0600

    fix: typo

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -3,8 +3,8 @@ export const VERSION_TEXT = "Version 2.0.0";
 export const Footer = () => {
   return (
     <div>
-      <div className="absolute bottom-2 left-2 text-xs text-white opacity-50">{VERSION_TEXT}</div>;
-      <div className="absolute bottom-2 right-2 text-xs text-white opacity-50">{COPYRIGHT_TEXT}</div>;
+      <div className="absolute bottom-2 left-2 text-xs text-white opacity-50">{VERSION_TEXT}</div>
+      <div className="absolute bottom-2 right-2 text-xs text-white opacity-50">{COPYRIGHT_TEXT}</div>
     </div>
   );
 };
